c16f01c49cd27210df039238a3fbfb4f71e1bf9d
zveridze <zverevdmitri.87@gmail.com>
2020-04-24 20:59:48 +0300

    Add readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+### How to start
+1. Clone repo
+2. pip install -r requirements.txt
+3. Run ```python3 trading_bot.py```   
--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 # Set up your service token here
-API_KEY = 'bq9bci7rh5rc96c0nvi0'
+API_KEY = ''
 # Finnhub api url
 FINNHUB_API_URL = 'https://finnhub.io/api/v1/stock/candle'
